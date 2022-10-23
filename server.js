a8e400da951dcd6d85134faac70601b85907fc8a
Mitchell Foote <mcfoote321@gmail.com>
2022-10-22 20:34:21 -0400

    base server functionality

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const boards = [
+    {id: 1, name: 'programming'},
+    {id: 2, name: 'gaming'},
+    {id: 3, name: 'music'},
+    {id: 4, name: 'video'}
+];
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Agora!');
 });
 
 app.get('/api/boards', (req, res) => {
-    res.send(['Programming', 'Gaming', 'Music', 'Video']);
+    res.send(boards);
 });
 
 app.get('/api/boards/:id', (req, res) => {
-    res.send(req.query);
+    let board = boards.find(b => b.id === parseInt(req.params.id));
+    if(!board) res.status(404).send('Board not found.');
+    res.send(board);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
