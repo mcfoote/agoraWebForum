c0c724c403dcfb5debb7158a3a33b07958a994cf
Mitchell Foote <mcfoote321@gmail.com>
2022-10-24 20:01:45 -0400

    index router

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const boardRoutes = require('./boardRoutes');
+const threadRoutes = require('./threadRoutes');
+const postRoutes = require('./postRoutes');
+
+router.use('/users', userRoutes);
+router.use('/boards', boardRoutes);
+router.use('/threads', threadRoutes);
+router.use('/posts', postRoutes);
+
+module.exports = router;
