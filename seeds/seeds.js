7207fb01f2c775e4b6750a53ddea1e83b49e529d
Mitchell Foote <mcfoote321@gmail.com>
2022-10-28 19:57:06 -0400

    update data and models

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -7,6 +7,12 @@ const seedDatabase = async() => {
 
     await sequelize.sync({ force: true });
 
+    for(const board of boardData) {
+        await Board.create({
+            ...board,
+            
+        });
+    }
 
     process.exit(0);
 };
