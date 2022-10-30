9e87948088d1fb50e141fc49ebc7e957bfb33ee1
Mitchell Foote <mcfoote321@gmail.com>
2022-10-29 23:39:58 -0400

    more work on models and controllers

--- controllers/api/boardRoutes.js
+++ controllers/api/boardRoutes.js
@@ -21,5 +21,30 @@ router.put('/:id', async(req, res) => {
 
 });
 
+router.delete('/:id', async(req, res) => {
+
+    try{
+        const boardData = await Board.destroy({
+            where: {
+               id: req.params.id,
+            },
+        });
+
+        if(!boardData) {
+            res.status(404).json({message: 'No board found with matching id.'});
+
+            return;
+        }
+
+        res.status(200).json(boardData);
+
+    } catch(err) {
+
+        res.status(500).json(err);
+
+    }
+
+});
+
 
 module.exports = router;
