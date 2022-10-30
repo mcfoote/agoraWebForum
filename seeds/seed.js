5140456bdb3d614480b9630e6edc0d340550d093
Mitchell Foote <mcfoote321@gmail.com>
2022-10-30 17:25:01 -0400

    expand seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,12 +2,21 @@ const sequelize = require('../config/connection.js');
 const { User, Board, Thread, Post } = require('../models');
 
 const boardData = require('./boardData.json');
+const threadData = require('./threadData.json');
+const postData = require('./postData.json');
+const userData = require('./userData.json');
 
 const seedDatabase = async() => {
 
     await sequelize.sync({ force: true });
 
     await Board.bulkCreate(boardData);
+
+    await Thread.bulkCreate(threadData);
+    
+    await Post.bulkCreate(postData);
+
+    await User.bulkCreate(userData);
 
     process.exit(0);
 };
