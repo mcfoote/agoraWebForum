a2175523296e9cd928823fab9166cd0e98f69e1f
Mitchell Foote <mcfoote321@gmail.com>
2022-10-30 23:43:01 -0400

    work on front end js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,19 +2,41 @@ const loginBtn = document.getElementById('loginSubmit');
 
 const accountCreateBtn = document.getElementById('accountSubmit');
 
+loginBtn.addEventListener('click', login());
+
 accountCreateBtn.addEventListener('click', registerAccount());
 
 function registerAccount(){
-    let userName = document.getElementById('inputUserName2');
-    let userEmail = document.getElementById('inputEmail2');
-    let userPass = document.getElementById('inputPassword2');
+    const userName = document.getElementById('inputUserName2').value;
+    const userEmail = document.getElementById('inputEmail2').value;
+    const userPass = document.getElementById('inputPassword2').value;
+    console.log(userName);
+    console.log(userEmail);
+    console.log(userPass);
 
     fetch('http://localhost:3001/api/users', {
         method: 'POST',
+        mode: 'no-cors',
         body: {
             user_name: userName,
             email: userEmail,
-            password: userPass
+            password: userPass,
+        },
+    });
+};
+
+function login(){
+    const userName = document.getElementById('inputUserName2').value;
+    const userPass = document.getElementById('inputPassword2').value;
+    console.log(userName);
+    console.log(userPass);
+
+    fetch('http://localhost:3001/api/users/login', {
+        method: 'POST',
+        mode: 'no-cors',
+        body: {
+            user_name: userName,
+            password: userPass,
         },
     });
 };
