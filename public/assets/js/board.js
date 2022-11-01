c977d5b898155c2d57a0105dd0da55406f324bbe
Mitchell Foote <mcfoote321@gmail.com>
2022-10-31 23:57:05 -0400

    more work on front end js

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -1,6 +1,7 @@
 const cardSpace = document.getElementById('cardSpace');
 const boardName = document.getElementById('boardTitle').innerHTML;
 const baseRoute = 'http://localhost:3001/api/threads/'
+const postBaseRoute = 'http://localhost:3001/api/posts/'
 let boardIdRoute;
 let boardID;
 
@@ -33,28 +34,41 @@ async function buildBoard() {
 
     for(let i = 0; i < threads.length; i++) {
         let threadID = threads[i].id;
+        let canvasName = 'threadCanvas'+threadID;
+        let postCanvasName = 'postCanvas'+threadID;
         cardSpace.innerHTML +=`
        <div class="card text-bg-secondary m-3" style="width: 18rem;">
             <img src="" class="card-img-top" alt="Placeholder img">
             <div class="card-body">
                 <h5 class="card-title">${threads[i].title}</h5>
                 <p class="card-text">${threads[i].thread_description}</p>
-                <a href="#" class="btn btn-success">Open Thread</a>
-            </div>
-        </div>
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <div>
-                    Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-                </div>
-    
+                    <a class="btn btn-success" data-bs-toggle="offcanvas" href="#${canvasName}" role="button" aria-controls="offcanvasExample">Open Thread</a>
+                        <div class="offcanvas offcanvas-start" tabindex="-1" id="${canvasName}" aria-labelledby="offcanvasExampleLabel">
+                            <div class="offcanvas-header">
+                                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body" id="${postCanvasName}">
+                            
+                            </div>
+                        </div>
             </div>
         </div>
         `;
+
+        let posts = [];
+        let postCanvas = document.getElementById(postCanvasName);
+        let threadIdRoute = postBaseRoute + threadID;
+        const response = await fetch(threadIdRoute);
+        posts = await response.json();
+
+        for(j = 0; j < posts.length; j++) {
+            postCanvas.innerHTML = `
+            <div>
+                ${posts[j].post_content}
+            </div>
+            `
+        }
        
     }
 
