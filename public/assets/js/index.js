ee1856c07f4309a8f999e375f1a302da289d4207
Mitchell Foote <mcfoote321@gmail.com>
2022-11-01 04:29:19 -0400

    working account registration/login function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,6 +5,8 @@ loginBtn.addEventListener('click', login);
 accountCreateBtn.addEventListener('click', registerAccount);
 
 async function registerAccount(){
+
+    event.preventDefault();
     const userName = document.getElementById('inputUserName2').value;
     const userEmail = document.getElementById('inputEmail2').value;
     const userPass = document.getElementById('inputPassword2').value;
@@ -14,15 +16,21 @@ async function registerAccount(){
 
     const response = await fetch('http://localhost:3001/api/users', {
         method: 'POST',
-        body: {
+        headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+        body: JSON.stringify({
             user_name: userName,
             email: userEmail,
             password: userPass,
-        },
+        }),
     });
 };
 
 async function login(){
+
+    event.preventDefault();
     const userName = document.getElementById('inputUserName2').value;
     const userPass = document.getElementById('inputPassword2').value;
     console.log(userName);
@@ -30,9 +38,14 @@ async function login(){
 
     const response = await fetch('http://localhost:3001/api/users/login', {
         method: 'POST',
-        body: {
+        headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+        body: JSON.stringify({
             user_name: userName,
             password: userPass,
-        },
+        }),
+
     });
 };
