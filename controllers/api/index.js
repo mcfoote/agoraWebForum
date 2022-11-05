b27dc4674ff2403f0dafd3615a06e0baf7fd0b0e
Mitchell Foote <mcfoote321@gmail.com>
2022-11-05 00:40:20 -0400

    rolling back deployment changes unable to get deployment working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,11 @@ const userRoutes = require('./userRoutes');
 const boardRoutes = require('./boardRoutes');
 const threadRoutes = require('./threadRoutes');
 const postRoutes = require('./postRoutes');
-const landingRoute = require('./landingRoute');
 
 router.use('/users', userRoutes);
 router.use('/boards', boardRoutes);
 router.use('/threads', threadRoutes);
 router.use('/posts', postRoutes);
-router.use('/', landingRoute);
+
 
 module.exports = router;
